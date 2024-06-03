635e18d90d345d64a34b4cf24ddd98450aa004c9
Bornit <alghenny@hotmail.com>
2024-06-03 19:03:23 -0400

    feat: Implemented Create and Get Products to the user-store api

---
+++ src/presentation/products/routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { AuthMiddleware } from '../middlewares/auth.middlewares';
+import { ProductController } from './controller';
+import { ProductService } from '../services';
+
+
+
+
+export class ProductsRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+      
+    const productService = new ProductService();
+
+    const controller = new ProductController(productService);
+    
+    // Definir las rutas
+    router.get('/', controller.getProducts );
+    router.post('/',[ AuthMiddleware.validateJWT ], controller.createProduct );
+
+
+
+    return router;
+  }
+
+
+}
+
